6d8a2b49a61e590559533b190d7da8cca1129768
Saru <raphaas@hotmail.com>
2025-05-30 17:49:07 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-const Home = () => {
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
   return (
-    <div>
-      <h1 className="text-emerald-500">Home</h1>
-    </div>
+   <div><Button>Raphael Saru</Button></div>
   );
 }
-
-
-
-export default Home;
+// This is a simple Next.js page that serves as the home page for the Tattoo Studio Management System.
